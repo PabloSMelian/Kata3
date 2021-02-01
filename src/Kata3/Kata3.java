32c69bed9970c25d2f5e1a774a982c044651dab5
Pablo Sánchez <pablosanmel05@gmail.com>
2021-02-01 21:39:27 +0100

    first commit

---
+++ src/Kata3/Kata3.java
@@ -0,0 +1,21 @@
+package Kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram();
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("outlook.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAMA", histogram);
+        histogramDisplay.execute();
+    }
+
+}
